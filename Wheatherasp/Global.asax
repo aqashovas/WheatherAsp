b61a6801c6926fae4f57c48dfbe92f5bb5980cb1
Sabina Aqashova <aqashova@inbox.ru>
2019-02-08 21:19:47 +0400

    First

---
+++ Wheatherasp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Wheatherasp.MvcApplication" Language="C#" %>
---
+++ Wheatherasp/Scripts/main.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function () {
+
+
+    $("select[name='Country']").change(function () {
+        var country = $(this).val();
+           
+
+
+        var url1 = "http://api.apixu.com/v1/current.json?key=f7688187a493412fb75160227190802&q=";
+        var url = url1.concat(country);
+        console.log(url);
+          
+           
+            $.ajax({
+                url: url,
+                type: "get",
+                dataType: "json",
+                success: function (response) {
+                    var date = new Date($.now());
+                    var nwdate = date.getDay();
+                    console.log(nwdate);
+                    var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+                    $(".wheatherlist").empty();
+                    $(".wheatherlist").append(`  <li class="list-group-item day ">Date: ` + dayNames[nwdate] + `</li>
+                    <li class="list-group-item borderless temp">Temperature: `+ response.current.temp_c + ` C°</li>
+                    <li class="list-group-item borderless wind">Wind: `+ response.current.wind_mph + ` MPH</li>`)
+                    console.log(response);
+
+                }
+            });
+           
+
+    });
+
+});
